9ecfc03b77cae8983564ff293ae68e8a2546cde2
Jaime Melis <jmelis@opennebula.org>
2014-07-03 12:36:42 +0200

    improve lxc-centos

--- bin/lxc-centos.sh
+++ bin/lxc-centos.sh
@@ -14,7 +14,15 @@ REPO=$(cd $(dirname `realpath $0`)/..; pwd)
 CONFIG=/var/lib/lxc/$NAME/config
 ROOTFS=/var/lib/lxc/$NAME/rootfs
 
+################################################################################
+# Create Container
+################################################################################
+
 sudo lxc-create -t download -n $NAME -- -d centos -r 6 -a amd64
+
+################################################################################
+# LXC Container Configuration File
+################################################################################
 
 sudo sed -i '/^lxc.network.type = empty/d' $CONFIG
 
@@ -24,17 +32,34 @@ lxc.network.link=br0
 lxc.network.flags=up
 lxc.network.hwaddrdd=$MAC
 lxc.network.ipv4=$IP/24
+lxc.network.ipv4.gateway=172.16.77.1
 EOF
+
+################################################################################
+# Other System Files
+################################################################################
+
+sed -i "/^${NAME}$/d" $REPO/ansible_playbooks/hosts
+echo "${NAME}" >> $REPO/ansible_playbooks/hosts
 
 sudo sed -i.bk.$(date '+%Y%m%d%H%M') "/$IP/d" /etc/hosts
 echo "$IP $NAME" | sudo tee -a /etc/hosts > /dev/null
+
+################################################################################
+# ROOTFS Filesystem
+################################################################################
 
 sudo mkdir -p $ROOTFS/root/.ssh
 cat $HOME/.ssh/id_dsa.pub | sudo tee -a $ROOTFS/root/.ssh/authorized_keys > /dev/null
 sudo chown -R root:root $ROOTFS/root/.ssh/
 
 sudo sed -i 's/dhcp/none/' $ROOTFS/etc/sysconfig/network-scripts/ifcfg-eth0
-
 sudo sed -i 's/^root.*/root:!:15800::::::/' $ROOTFS/etc/shadow
+echo "UseDNS no" | sudo tee -a $ROOTFS/etc/ssh/sshd_config > /dev/null
+echo "nameserver 172.16.77.1" | sudo tee $ROOTFS/etc/resolv.conf > /dev/null
 
-echo "${NAME}" >> $REPO/ansible_playbooks/hosts
+################################################################################
+# Start Container
+################################################################################
+
+sudo lxc-start -d -n $NAME
