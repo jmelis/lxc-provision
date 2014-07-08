9208a2f30ab05da70f12fc6923e654c344ec2401
Jaime Melis <j.melis@gmail.com>
2014-07-08 12:32:19 +0200

    Update provision.sh
    Adds comments to the full provision script

--- provision.sh
+++ provision.sh
@@ -6,15 +6,17 @@ NAME=$1
 IP=$2
 COMMIT=${3-master}
 
+# Provision an empty container
 $TOPDIR/bin/lxc-centos.sh $NAME $IP
-sudo lxc-start -d -n $NAME
 
+# Start the container
+sudo lxc-start -d -n $NAME
 sleep 10
 
+# Install OpenNebula build dependencies, users, and developer tools (vim, etc)
 cd $TOPDIR/ansible_playbooks
 ansible-playbook -i ${NAME}, site.yml
 
+# Compile and Install OpenNebula
 cd $TOPDIR/scripts
 cat install-opennebula.sh | ssh oneadmin@${NAME} bash -s $COMMIT
-
-
